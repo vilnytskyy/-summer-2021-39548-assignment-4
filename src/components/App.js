7df16a2a62265a026d25616013398a7835577a60
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-01 19:04:22 -0400

    Passed prop to Home component

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            accountBalance: 14568.27
+        }
+    }
+
     render() {
+        const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />);
+
         return (
             <Router>
                 <Switch>
-                    <Route exact path="/" component={Home} />
+                    <Route exact path="/" render={HomeComponent} />
                 </Switch>
             </Router>
         );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import AccountBalance from './AccountBalance';
 import bank from '../imgs/piggybank.png';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <img src={bank} alt="piggybank"/>
+                <img src={bank} alt="piggybank" />
                 <h1>Bank of React</h1>
+
+                <AccountBalance accountBalance={this.props.accountBalance} />
             </div>
         );
     }
