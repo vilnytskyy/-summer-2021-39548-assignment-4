395bb1eb59700aabd41c4dcb9944b822527a6736
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-01 19:21:22 -0400

    Linked UserProfile page to Home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './Home';
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
 
 class App extends Component {
     constructor() {
         super();
 
         this.state = {
-            accountBalance: 14568.27
+            accountBalance: 14568.27,
+            currentUser: {
+                userName: 'joe_shmo',
+                memberSince: '07/23/96'
+            }
         }
     }
 
     render() {
         const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />);
+        const UserProfileComponent = () => (
+            <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
+        );
 
         return (
             <Router>
                 <Switch>
                     <Route exact path="/" render={HomeComponent} />
+                    <Route exact path="/userProfile" render={UserProfileComponent} />
                 </Switch>
             </Router>
         );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AccountBalance from './AccountBalance';
+import { Link } from 'react-router-dom';
 import bank from '../imgs/piggybank.png';
 
 class Home extends Component {
@@ -8,6 +9,8 @@ class Home extends Component {
             <div>
                 <img src={bank} alt="piggybank" />
                 <h1>Bank of React</h1>
+
+                <Link to="/userProfile">User Profile</Link>
 
                 <AccountBalance accountBalance={this.props.accountBalance} />
             </div>
