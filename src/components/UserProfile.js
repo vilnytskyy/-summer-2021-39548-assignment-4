1a11d6df791733c9b42db4f1edf12cfa3b4d2b86
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-02 17:21:43 -0400

    Linked Home page to UserProfile page

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class UserProfile extends Component {
     render() {
@@ -8,6 +9,8 @@ class UserProfile extends Component {
 
                 <div>Username: {this.props.userName}</div>
                 <div>Member Since: {this.props.memberSince}</div>
+                
+                <Link to="/">Return to Home</Link>
             </div>
         );
     }
