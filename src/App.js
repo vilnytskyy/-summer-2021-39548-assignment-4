5adbbcdae2e7354131eeba929aae1cceef8c88c0
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-02 17:55:44 -0400

    Added mock login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
+import LogIn from './components/LogIn';
 
 class App extends Component {
     constructor() {
@@ -16,17 +17,25 @@ class App extends Component {
         }
     }
 
+    mockLogIn = (logInInfo) => {
+        const newUser = { ...this.state.currentUser }
+        newUser.userName = logInInfo.userName
+        this.setState({ currentUser: newUser })
+    }
+
     render() {
         const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />);
         const UserProfileComponent = () => (
             <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
         );
+        const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />);
 
         return (
             <Router>
                 <Switch>
                     <Route exact path="/" render={HomeComponent} />
                     <Route exact path="/userProfile" render={UserProfileComponent} />
+                    <Route exact path="/login" render={LogInComponent} />
                 </Switch>
             </Router>
         );
