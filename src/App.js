869175a1e4ae2e05e5aed7a89f7e6fd77555c450
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-05 23:02:28 -0400

    Created fetchDebitData and fetchCreditData, initialized componentDidMount, updated currentUser

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 import './css/App.css';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
@@ -15,7 +16,7 @@ class App extends Component {
             accountBalance: 14568.27,
             currentUser: {
                 userName: 'vilnytskyy',
-                memberSince: '08/22/18'
+                memberSince: '06/27/21'
             },
             debits: [
                 {
@@ -32,7 +33,9 @@ class App extends Component {
                     amount: "credit-amount",
                     date: "credit-date"
                 }
-            ]
+            ],
+            debitsFound: false,
+            creditsFound: false
         }
     }
 
@@ -44,14 +47,51 @@ class App extends Component {
         
     }
 
-    componentDidMount() {
+    fetchDebitData = async () => {
+        try {
+            let response = await axios.get("https://moj-api.herokuapp.com/debits");
+            this.setState({ debits: response.data, debitsFound: true });
+        } catch (error) {
+            if (error.response) {
+                /*
+                 * The request was made and the server responded with a
+                 * status code that falls out of the range of 2xx
+                 */
+                console.log(error.response.data); //Not Found
+                console.log(error.response.status); //404
+                this.setState({ debitsFound: false });
+            }
+        }
+    }
 
+    fetchCreditData = async () => {
+        try {
+            let response = await axios.get("https://moj-api.herokuapp.com/credits");
+            this.setState({ credits: response.data, creditsFound: true });
+        } catch (error) {
+            if (error.response) {
+                /*
+                 * The request was made and the server responded with a
+                 * status code that falls out of the range of 2xx
+                 */
+                console.log(error.response.data); //Not Found
+                console.log(error.response.status); //404
+                this.setState({ creditsFound: false });
+            }
+        }
+    }
+
+    componentDidMount() {
+        this.fetchDebitData();
+        this.interval = setInterval(() => this.fetchDebitData(), 60 * 1000);
+        this.fetchCreditData();
+        this.interval = setInterval(() => this.fetchCreditData(), 60 * 1000);
     }
 
     mockLogIn = (logInInfo) => {
-        const newUser = { ...this.state.currentUser }
-        newUser.userName = logInInfo.userName
-        this.setState({ currentUser: newUser })
+        const newUser = { ...this.state.currentUser };
+        newUser.userName = logInInfo.userName;
+        this.setState({ currentUser: newUser });
     }
 
     render() {
@@ -60,8 +100,8 @@ class App extends Component {
             <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
         );
         const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />);
-        const CreditsComponent = () => (<Credits />);
-        const DebitsComponent = () => (<Debits debitInfo={this.state.debits}/>);
+        const CreditsComponent = () => (<Credits creditInfo={this.state.credits} accountBalance={this.state.accountBalance}/>);
+        const DebitsComponent = () => (<Debits debitInfo={this.state.debits} accountBalance={this.state.accountBalance}/>);
 
         return (
             <Router>
