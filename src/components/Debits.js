a98ee091f140aa5926179291004072ce6cacfa85
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-05 21:23:51 -0400

    Created debit display

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import debit from '../imgs/debit.png';
+import Display from './Display';
 
 class Debits extends Component {
     render() {
+        const debitRow = this.props.debitInfo.map((temp) =>
+            <Display data={temp} key={temp.id}/>
+            // <tr key={temp.id}>
+            //     <td>{temp.id}</td>
+            //     <td>{temp.description}</td>
+            //     <td>{temp.amount}</td>
+            //     <td>{temp.date}</td>
+            // </tr>
+        );
+
         return (
             <div className="Debits">
                 <div className="Debits-header">
@@ -12,6 +24,20 @@ class Debits extends Component {
                 </div>
 
                 <Link className="link" id="home" to="/">Return to Home</Link>
+
+                <div>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>ID</td>
+                                <td>Description</td>
+                                <td>Amount</td>
+                                <td>Date</td>
+                            </tr>
+                            {debitRow}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
